d429eba26498de246dfcc339ac0a8ffd57dee2ba
OscarLozano01 <50722140+OscarLozano01@users.noreply.github.com>
2019-11-25 12:53:34 -0500

    Delete js.js

--- js/js.js
+++
@@ -1,9 +0,0 @@
-document.write('<style type="text/css">div.cp_oculta{display: none;}</style>'); 
-function MostrarOcultar(capa,enlace) 
-{ 
-    if (document.getElementById) 
-    { 
-        var aux = document.getElementById(capa).style; 
-        aux.display = aux.display? "":"block"; 
-    } 
-} 
